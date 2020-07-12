7c07a57d07d4eba9ef650baf1a0fdfc383410c93
pikerstrow <pikerstrow@gmail.com>
2020-07-12 09:54:07 +0300

    Buffering added for better visual testing

--- app/Http/Controllers/Parser/ParserController.php
+++ app/Http/Controllers/Parser/ParserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Parser\AuctionCarsJsonParser;
 use App\Parser\AuctionsParser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 
 class ParserController extends Controller
@@ -15,9 +16,23 @@ class ParserController extends Controller
     {
         //TODO:: don't forget remove buffering after task finish.
         try {
+            if (ob_get_level() == 0) ob_start();
+
+            echo "Parser started </br>" . PHP_EOL;
+            ob_flush();
+            flush();
+
+
             $parser = new AuctionsParser();
             $auctions_data = $parser->parse();
+
+
             $test = [];
+
+            echo "Auctions JSON cars parser start! </br>" . PHP_EOL;
+            ob_flush();
+            flush();
+
             foreach ($auctions_data as $item) {
                 $auction = Auction::updateOrCreate(
                     ['auction_id' => $item['auction_id']],
@@ -26,11 +41,21 @@ class ParserController extends Controller
                 $cars_parser = new AuctionCarsJsonParser();
                 $auction_cars_data = $cars_parser->parse($auction->url);
                 $test[] = $auction_cars_data;
+
+                echo "Auction {$auction->city} cars parsed! </br>" . PHP_EOL;
+                ob_flush();
+                flush();
             }
+
+            echo "Done!!!";
+            ob_end_flush();
+
+            
             dd($test);
         } catch (\Throwable $exception) {
-            //TODO::Logging
-            dd($exception->getMessage() . ' ' . $exception->getFile() . ' ' . $exception->getLine());
+            Log::useDailyFiles(storage_path().'/logs/parser.log');
+            Log::info(['error' => $exception->getMessage() . ' ' . $exception->getFile() . ' ' . $exception->getLine()]);
+//            dd($exception->getMessage() . ' ' . $exception->getFile() . ' ' . $exception->getLine());
         }
     }
 }
